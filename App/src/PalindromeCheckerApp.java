1e602bfbf1cfe129c0b6b7b6c6a9d8949befdf47
srijothikaa19 <srijothikaa@gmail.com>
2026-03-12 21:56:36 +0530

    Print a Hardcoded Palindrome Result

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,10 +1,29 @@
 
+
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully.");
+        String text = "madam";
+        text = text.toLowerCase();
+
+        boolean isPalindrome = true;
+        int left = 0;
+        int right = text.length() - 1;
+
+        while (left < right) {
+            if (text.charAt(left) != text.charAt(right)) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+
+        if (isPalindrome) {
+            System.out.println(text + " is a Palindrome.");
+        } else {
+            System.out.println(text + " is NOT a Palindrome.");
+        }
     }
 }
